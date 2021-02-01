877e07630bf9200b3a463ab117c82464039ede74
suhanabhargava <76730859+suhanabhargava@users.noreply.github.com>
2021-02-01 21:48:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,13 +3,13 @@ var cat , catImg1, catImg2, catImg3 , mouse , mouseImg1, mouseImg2 , mouseImg3;
 
 function preload() {
     //load the images here
-  backgroundImg = loadImage("images/garden.png");
-  catImg1 = loadAnimation("images/cat1.png");
-  catImg2 = loadAnimation("images/cat2.png","images/cat3.png" );
-  catImg3 = loadAnimation("images/cat4.png");
-  mouseImg1 = loadAnimation("images/mouse1.png");
-  mouseImg2 = loadAnimation("images/mouse2.png", "images/mouse3.png");
-  mouseImg3 = loadAnimation("images/mouse4.png");
+  backgroundImg = loadImage("garden.png");
+  catImg1 = loadAnimation("cat1.png");
+  catImg2 = loadAnimation("cat2.png","cat3.png" );
+  catImg3 = loadAnimation("cat4.png");
+  mouseImg1 = loadAnimation("mouse1.png");
+  mouseImg2 = loadAnimation("mouse2.png", "mouse3.png");
+  mouseImg3 = loadAnimation("mouse4.png");
   
   
   
@@ -70,3 +70,4 @@ function keyPressed(){
 
 
 }
+
